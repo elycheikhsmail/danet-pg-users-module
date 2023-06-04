990b882ce2565cfa4dc3d6e747dbff69417bed2a
ely cheikh smail <eouldsmai@gmail.com>
2023-06-04 10:30:43 +0000

    remove unused module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
-import { Module } from 'danet/mod.ts';
-import { TodoModule } from './todo/module.ts';
-import { AppController } from './app.controller.ts';
-import { ArticleModule } from './articles/module.ts';
+import { Module } from 'danet/mod.ts'; 
+import { AppController } from './app.controller.ts'; 
 import { UserModule } from './users/users.module.ts';
 
 @Module({
   controllers: [AppController],
-  imports: [TodoModule, ArticleModule, UserModule],
+  imports: [UserModule],
 })
 export class AppModule {}
--- src/scripts/init-db.service.ts
+++ src/scripts/init-db.service.ts
@@ -9,33 +9,17 @@ export class InitDbService extends DbConfig {
     await this.onAppBootstrap();
     await this.client.queryObject(
       `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-      CREATE TABLE IF NOT EXISTS todo (
-        _id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        content TEXT
-      );`,
-    );
-
-    await this.client.queryObject(
-      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-      CREATE TABLE IF NOT EXISTS articles (
-        _id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-        title VARCHAR NOT NULL,
-        content TEXT,
-        price INTEGER
-      );`,
-      // later I will add the following colomuns
-      // categorie Integer,
-      // subcategorie Integer
-      // image_link TEXT
-    );
-
-    await this.client.queryObject(
-      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
       CREATE TABLE IF NOT EXISTS users (
         _id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
         email TEXT,
         password TEXT
+      );`,
+    );
+    await this.client.queryObject(
+      `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+      CREATE TABLE IF NOT EXISTS tokens (
+        _id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        token_string TEXT
       );`,
     );
 
