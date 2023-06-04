78b36c01ec891bf0f590577802c5b98e5b23ba5d
ely cheikh smail <eouldsmai@gmail.com>
2023-06-04 14:23:01 +0000

    add expire date to the token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,7 +15,7 @@ export class UserController {
   }
 
   @Get('logout')
-  async logoutUser(@Req() req: Request) { 
+  async logoutUser(@Req() req: Request) {
     return this.userService.logoutUser(req);
   }
 
--- src/users/utiles.ts
+++ src/users/utiles.ts
@@ -1,4 +1,4 @@
-import jwt from 'npm:jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { Request } from 'oak/mod.ts';
 
 export function get_token_from_request(request: Request): string {
@@ -13,7 +13,11 @@ export function get_token_from_request(request: Request): string {
 }
 
 export function verify_token(token: string) {
+  const options = {
+    expiresIn: Deno.env.get('EXPIRE_IN'),
+    //'1h', // Durée de validité du token (1 heure dans cet exemple)
+  };
   const SECKRET_KEY = Deno.env.get('SECKRET_KEY');
-  const jwtPayload = jwt.verify(token, SECKRET_KEY);
+  const jwtPayload = jwt.verify(token, SECKRET_KEY, options);
   return jwtPayload;
 }
